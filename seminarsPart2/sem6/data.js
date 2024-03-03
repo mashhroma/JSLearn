1ed751f8c9a2dcd205c216aa6bae9c032a2bc1e0
mashhroma <mashh@mail.ru>
2024-03-03 14:15:40 +0300

    seminar 7

---
+++ seminarsPart2/sem6/data.js
@@ -0,0 +1,26 @@
+const dataProducts = `
+[
+    {
+        "id": 1,
+        "name": "t-shirt",
+        "brand": "Mango People",
+        "price": 300,
+        "currency": "USD",
+        "color": "red",
+        "size": "XL",
+        "quantity": 2,
+        "image": "./images/prod_1.jpeg"
+    },
+    {
+        "id": 2,
+        "name": "capsule",
+        "brand": "Ellery X M'O",
+        "price": 52,
+        "currency": "USD",
+        "color": "black",
+        "size": "XL",
+        "quantity": 1,
+        "image": "./images/prod_2.jpeg"
+    }
+]
+`;
