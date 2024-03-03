9280a92bbf1e99289d929b675a3952af7db5ae69
mashhroma <mashh@mail.ru>
2024-03-03 14:46:08 +0300

    hw6

---
+++ homeworksPart2/shop_project/data.js
@@ -0,0 +1,52 @@
+const dataProducts = `
+[
+    {
+        "href": "product.html",
+        "src": "./img/prod_1.png",
+        "title": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "currency": "$",
+        "price": 52
+    },
+    {
+        "href": "product.html",
+        "src": "./img/prod_2.png",
+        "title": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "currency": "$",
+        "price": 52
+    },
+    {
+        "href": "product.html",
+        "src": "./img/prod_3.png",
+        "title": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "currency": "$",
+        "price": 52
+    },
+    {
+        "href": "product.html",
+        "src": "./img/prod_4.png",
+        "title": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "currency": "$",
+        "price": 52
+    },
+    {
+        "href": "product.html",
+        "src": "./img/prod_5.png",
+        "title": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "currency": "$",
+        "price": 52
+    },
+    {
+        "href": "product.html",
+        "src": "./img/prod_6.png",
+        "title": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "currency": "$",
+        "price": 52
+    }
+]
+`;
